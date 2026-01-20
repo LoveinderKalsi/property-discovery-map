a66ccabdd3d8d7a4f16a13ccf5958ac8edd7da73
LoveinderKalsi <Kalsijatt1496@gmail.com>
2026-01-20 21:03:55 +0530

    remove invalid validator file causing build error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig:NextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   distDir: "build",
   images: {
     remotePatterns: [
